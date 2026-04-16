86064be8b414c6f6e4a48f4d9142f21ba4e26901
Ram Suryansh <ram.suryansh@hotmail.com>
2026-04-16 23:59:26 +0530

    style: enhance styling of MovieList component for better presentation

--- 05-hoc/src/movies/movie-list.tsx
+++ 05-hoc/src/movies/movie-list.tsx
@@ -8,9 +8,9 @@ function MovieList({ data }: MovieListProps) {
   return (
     <div className='p-4'>
       <h2 className='text-xl font-bold mb-2'>🎞️ Movie List</h2>
-      <ul className='space-y-1'>
+      <ul className='space-y-1 border rounded bg-gray-50'>
         {data.map((movie) => (
-          <li key={movie.id} className='border-b pb-1'>
+          <li key={movie.id} className='border-b pb-1 px-2'>
             {movie.title} ({movie.year})
           </li>
         ))}
